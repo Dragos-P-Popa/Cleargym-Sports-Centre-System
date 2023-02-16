21a180fba3a1b31f2fd5fce31ec1093f7363a30d
Dragos Popa <pdragos66@gmail.com>
2023-02-16 19:54:14 +0000

    '/users' endpoint
    GET and POST functions for the '/users' endpoint. Non-functional.

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5001;                  //Port that the node app will be listening to
 
-app.get('/', (req, res) => {        //GET requests to the root ("/") will route here
-    console.log(`Request recieved`); 
+
+app.get('/users', (req, res) => {
+    res.status(200).json({ action: "list all users" }); 
 });
+
+app.post('/users', (req, res) => {
+    res.status(200).json({ action: "create new user" }); 
+})
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`); 
