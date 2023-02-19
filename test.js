708831fa8dc6fa24cf5c35489654222ba700ab6b
alialmoharif <ml19asaa@leeds.ac.uk>
2023-02-19 14:25:42 +0000

    Additional tests

--- test.js
+++ test.js
@@ -26,7 +26,7 @@ describe("Testing",function (){
         .post('/users')
         .send({"firstName": "testCase001",
             "lastName": "testCase001",
-            "email": "testCase01@squad007.com",
+            "email": "testcase001@squad007.com",
             "password": "test@case001"})
 
         //This means the record has been created
@@ -42,7 +42,59 @@ describe("Testing",function (){
         });
     });
 
-//Test2: Delete the user that has been created
+//Test 2: Get the user details by GET request
+        it("Get the user details by the user ID", function (done){
+
+            server
+                .get('/users/'+userId)
+
+                //This means that there is no record of that user which means deletion succeed
+                .expect(200)
+                .end(function (err,res) {
+                    res.status.should.equal(200);
+                    done();
+
+                })
+        })
+
+
+
+//Test 3: update the user details by PATCH request
+        it("Update user details",function (done) {
+
+
+            server
+                .patch('/users/'+userId)
+                .send({"firstName": "UpdatedName",
+                    "lastName": "testCase001",
+                    "email": "testcase001@squad007.com"
+                })
+
+                //This means the record has been updated
+                .expect(204)
+                .end(function (err,res){
+                    res.status.should.equal(204);
+                    done();
+                });
+        });
+//Test 4: Get all the users that has been created if the privilege is high
+        it("Get all the users in the Database", function (done){
+
+            server
+                .get('/users')
+
+                //This means that there is no record of that user which means deletion succeed
+                .expect(200)
+                .end(function (err,res) {
+                    res.status.should.equal(200);
+                    done();
+
+                })
+        })
+
+
+
+//Test 5: Delete the user that has been created
 
 
     it("Delete the user by the user ID", function (done){
@@ -58,6 +110,8 @@ describe("Testing",function (){
 
             })
     })
+
+
 
     }
 
