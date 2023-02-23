d6a94085908dc004b63e8a1dadd24b6e3bb74c1c
Dragos Popa <pdragos66@gmail.com>
2023-02-23 15:31:30 +0000

    JWT refresh tokens
    Modified the `/login` endpoint to create a shorter lifetime access
    token with a longer lifetime refresh token.

--- auth/controllers/authentification.controller.js
+++ auth/controllers/authentification.controller.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 /*
-    This file creates the JWT token
+    This file creates the JWT tokens
 */
 
 // open the private key file
@@ -11,20 +11,32 @@ var privateKEY  = fs.readFileSync('private.key', 'utf8');
 exports.login = (req, res) => {
 
     // set the token settings
-    var signOptions = {
-    issuer:  'Sports Centre System',
-    expiresIn:  "12h",
-    // the audience will be checked to make sure the 
-    // correct user is making the request using a token which was 
-    // assigned to them
-    audience: req.body.email,
-    algorithm:  "RS256" 
-    };
+    var signOptionsAccess = {
+        issuer:  'Sports Centre System',
+        expiresIn:  "1h",
+        // the audience will be checked to make sure the 
+        // correct user is making the request using a token which was 
+        // assigned to them
+        audience: req.body.email,
+        algorithm:  "RS256" 
+        };
 
-    // create token containing user ID and the signOptions
+    // set the token settings
+    var signOptionsRefresh = {
+        issuer:  'Sports Centre System',
+        expiresIn:  "12h",
+        // the audience will be checked to make sure the 
+        // correct user is making the request using a token which was 
+        // assigned to them
+        audience: req.body.email,
+        algorithm:  "RS256" 
+        };
+
+    // create tokens containing user ID and the signOptions
     // alongside expiry and issue time
-    var token = jwt.sign({user: req.body.id}, privateKEY, signOptions);
+    var accessToken = jwt.sign({user: req.body.id}, privateKEY, signOptionsAccess);
+    var refreshToken = jwt.sign({user: req.body.id}, privateKEY, signOptionsRefresh);
 
-    // send the token
-    res.status(201).send({accessToken: token})
+    // send the tokens
+    res.status(201).send({accessToken: accessToken, refreshToken: refreshToken})
 }
