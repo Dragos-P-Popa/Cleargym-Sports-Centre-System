898841e6f26c89a0b21ace1326c49b41e51dcb8a
Yvana Qiu <yvana.qiu@gmail.com>
2023-02-28 01:09:54 +0000

    file renamed and tests format updated to receive two return values

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest 
-from facilities import app
+from app import app
 from datetime import datetime
 
 class FacilitiesTestCase(unittest.TestCase):
@@ -21,51 +21,58 @@ class FacilitiesTestCase(unittest.TestCase):
 
     def test_add_new_facility(self):
         #testing by adding a new facility with the right data
-        status_code = app.create_facility('Swimming pool', 30, 10.00, '8:00:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
+        result, status_code = app.create_facility('Swimming pool', 30, 10.00, '8:00:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
         # self.assertIsInstance(facility_id, int)
         self.assertEqual(status_code, 200) #200 - ok 201- accepted
 
         #testing by adding invalid capacity
         with self.assertRaises(TypeError):
-            status_code = app.create_facility('Swimming pool', 'A' , 10.00, '8:00:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
+            result, status_code = app.create_facility('Swimming pool', 'A' , 10.00, '8:00:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
             self.assertEqual(status_code, 400) #400 Bad request
         
         #testing by adding invalid price
         with self.assertRaises(TypeError):
-            status_code = app.create_facility('Swimming pool', 30, 'B', '8:00:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
+            result, status_code = app.create_facility('Swimming pool', 30, 'B', '8:00:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
             self.assertEqual(status_code, 400) #400 Bad request
 
         #testing by adding invalid opening time
         with self.assertRaises(TypeError):
-            status_code = app.create_facility('Swimming pool', 30, 10.00, '8A:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
-            self.assertEqual(status_code, 400) #400 Bad request
+            result, status_code = app.create_facility('Swimming pool', 30, 10.00, '8A:00', '10:00:00', '63F0DE4724EF6726E1F27D57')
+            result, self.assertEqual(status_code, 400) #400 Bad request
 
         #testing by adding invalid closing time
         with self.assertRaises(TypeError):
-            status_code = app.create_facility('Swimming pool', 30, 10.00, '8:00:00', '10:0ABC', '63F0DE4724EF6726E1F27D57')
+            result, status_code = app.create_facility('Swimming pool', 30, 10.00, '8:00:00', '10:0ABC', '63F0DE4724EF6726E1F27D57')
             self.assertEqual(status_code, 400) #400 Bad request
 
     def test_get_facility(self):
         #testing get facility by name 
-        status_code = app.get_facility("Swimming Pool")
+        result, status_code = app.get_facility("Swimming Pool")
         self.assertEqual(status_code, 200)
 
         #testing get facility by ID
-        status_code = app.get_facility("ID")
+        result, status_code = app.get_facility("ID")
         self.assertEqual(status_code, 200)
 
         #testing get facility by invalid ID i.e: too long
         with self.assertRaises(TypeError):
-            status_code = app.get_facility("123abc45de")
+            result, status_code = app.get_facility("123abc45de")
             self.assertEqual(status_code, 400)
 
         #testing get facility by non existing name
-        status_code = app.get_facility("non existing")
+        result, status_code = app.get_facility("non existing")
         self.assertEqual(status_code, 404)
 
         #testing get facility by non existing ID 
-        status_code = app.get_facility("09876543210")
+        result, status_code = app.get_facility("09876543210")
         self.assertEqual(status_code, 404)
 
+    def test_update_facility(self):
+        #test update facility with name
+        result, status_code = app.update_facility("Swimming Pool")
+        self.assertEqual(status_code, 200)
 
-    #def edit_facility_capacity(self):
+        #test update facility wih ID
+
+if __name__ == '__main__':
+    unittest.main()
