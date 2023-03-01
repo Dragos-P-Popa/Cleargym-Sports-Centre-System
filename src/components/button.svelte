8ecee4f01974a0c17f518df4f4a4375ea5d57292
Ali Almoharif <51306868+ASHS21@users.noreply.github.com>
2023-03-01 21:09:35 +0000

    button events implemented

---
+++ src/components/button.svelte
@@ -0,0 +1,13 @@
+<script>
+
+    let buttonProps = {
+        class:[$$restProps.class]
+    }
+</script>
+<button {...buttonProps}
+        on:click
+        on:mouseenter
+        on:mouseleave
+        on:mouseover>
+    <slot/>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
-  import MainButton from "../components/button.svelte"
+  import   MainButton from "../components/mainButton.svelte"
+  import  SecondaryButton from "../components/secondaryButton.svelte"
+  import button from "../components/button.svelte";
+
+
+
 </script>
 
 <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <MainButton>Login</MainButton>
-
+<SecondaryButton >Register</SecondaryButton>
 
