bdfe178e523385f7332ff3b6af7385928075cf32
Dragos Popa <pdragos66@gmail.com>
2023-03-02 20:52:10 +0000

    Initial dashboard layout
    Initial layout of the dashboard with a simple navbar implementation

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,7 +1,19 @@
-<h1 class="text-3xl font-bold underline">Welcome to the home screen</h1>
+<div class="grid grid-cols-12">
+    <div class="min-h-screen border-r-2 border-borderColor col-span-2 mr-2 p-8 pt-16">
+       <img class="place-self-center pr-3 mb-16" src = "logo.svg" alt="logo"/>
 
-<style lang="postcss">
-  :global(hmtl) {
-      background-color: theme(colors.gray.100);
-    }
-</style>
+       <button class="py-2 px-2 my-1 bg-[#EEEEF2] transition-colors duration-200 border-[#DDDDDD] border-[1px] rounded-lg w-full text-left inline-flex items-center">
+            <img class="pr-3" src = "home.svg" alt="home icon"/>
+            <span>Home</span>
+       </button> <br>
+       <button class="py-2 px-2 my-1 hover:bg-[#EEEEF2] transition-colors duration-200 hover:border-[#DDDDDD] border-[#FFFFFF] border-[1px] rounded-lg w-full text-left inline-flex items-center">
+            <img class="pr-3" src = "calendar.svg" alt="calendar icon"/>
+            <span>Bookings</span>
+       </button>
+
+
+    </div>
+    <div>
+        <p>MAIN</p> 
+     </div>
+</div>
