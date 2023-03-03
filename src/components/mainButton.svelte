2f03755b5c6e54f374b137926aac0ccf9f91d939
Dragos Popa <pdragos66@gmail.com>
2023-03-03 16:55:30 +0000

    Button component fixes
    Fixed the sizing for MainButton and SecondaryButton.

--- src/components/mainButton.svelte
+++ src/components/mainButton.svelte
@@ -1,6 +1,6 @@
 <script>
     let buttonProps = {
-        class:[$$restProps.class] + " py-2 my-2 w-3/5 min-w-xl rounded-lg text-white font-extrabold text-xl bg-mainBlue drop-shadow-md",
+        class:[$$restProps.class] + " py-2 my-2 min-w-xl rounded-lg text-white font-extrabold text-xl bg-mainBlue drop-shadow-md",
     }
 </script>
 <button {...buttonProps}
