24d1a1621297dde446cd0dceee2e1618fc2713a2
Dragos Popa <pdragos66@gmail.com>
2023-03-03 16:56:51 +0000

    Changed how the booking card works

--- src/components/bookingCard.svelte
+++ src/components/bookingCard.svelte
@@ -3,9 +3,17 @@
         class:[$$restProps.class] + " p-4 border-[1px] border-borderColor shadow-md rounded-lg bg-[url('/gym.jpeg')] pb-12"
     }
 
+    /**
+     * @type {String}
+     */
+    export let heading;
+    /**
+     * @type {String}
+     */
+    export let subheading;
 </script>
 
 <div  {...divProps}>
-    <p class="text-xl text-white font-extrabold"><slot name="heading"/></p>
-    <p class="text-sm text-white"><slot name="subheading"/></p>
+    <p class="text-xl text-white font-extrabold">{heading}</p>
+    <p class="text-sm text-white">{subheading}</p>
 </div>
