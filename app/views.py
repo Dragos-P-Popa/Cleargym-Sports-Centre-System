d670b49070050cf0d4ca58d12b39ebe732e66927
Patryk Rosowski [sc21ppr] <sc21ppr@leeds.ac.uk>
2023-03-05 20:33:00 +0000

    Complete first iteration of endpoints and tests

--- app/views.py
+++ app/views.py
@@ -1,29 +1,55 @@
 from app import app, db, models
-from flask import json, request, jsonify
+from flask import request, jsonify
 import datetime
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.exc import UnmappedInstanceError
 from datetime import datetime
 
-# The error handlers for IntegrityError and KeyError, which might occur when
-# data is missing or invalid
+# The error handlers for IntegrityError, KeyError, UnmappedInstanceError,
+# TypeError and AttributeError.
+# These might occur when data is:
+# - of an invalid type from the database perspective
+# - missing from the request
+# - refers to a non-existent database record
+# - of an invalid type from a function parameter perspective (e.g. strptime())
+# - refering to a non-existent attribute, e.g. in a NoneType object
+
 @app.errorhandler(IntegrityError)
 def integrity_error_handler(error):
     db.session.rollback()
     app.logger.error(str(error))
-    return jsonify({"Error": "Invalid or missing data in one or more fields"}), 400
+    # IntegrityError refers to invalid data type in database operations
+    return jsonify({"IntegrityError": "Invalid data in one or more fields"}), 400
 
 @app.errorhandler(KeyError)
 def key_error_handler(error):
     db.session.rollback()
     app.logger.error(str(error))
-    return jsonify({"Error": "No data in the request"}), 400
+    # KeyError refers to a missing key:value pair
+    return jsonify({"KeyError": "Missing data in the request"}), 400
 
 @app.errorhandler(UnmappedInstanceError)
 def unmapped_error_handler(error):
     db.session.rollback()
     app.logger.error("UnmappedInstanceError detected")
-    return jsonify({"Error": "Invalid ID in the request"}), 400
+    # UnmappedInstanceError is raised when trying to operate on a non-existent record
+    return jsonify({"UnmappedInstanceError": "Record not found in the database"}), 400
+
+@app.errorhandler(TypeError)
+def unmapped_error_handler(error):
+    db.session.rollback()
+    app.logger.error("TypeError detected")
+    # TypeError refers to invalid data type passed as an argument to a function
+    return jsonify({"TypeError": "Endpoint function operating on a wrong data type"}), 400
+
+@app.errorhandler(AttributeError)
+def unmapped_error_handler(error):
+    db.session.rollback()
+    app.logger.error("AttributeError detected")
+    # AttributeError is raised when trying to access an attribute
+    # of a NoneType object assigned to a variable in views.py
+    return jsonify({"AttributeError": "The referenced record does not exist"}), 400
+
 
 # This function is to create a new booking
 @app.route('/booking', methods=['POST'])
@@ -49,6 +75,17 @@ def post_booking():
         db.session.add(booking)
         db.session.commit()
 
+        # Create a new response
+        response = {'id': booking.id,
+                    'userId': booking.userId,
+                    'createDate': booking.createDate.strftime('%Y/%m/%d'),
+                    'bookingDate': booking.bookingDate.strftime('%Y/%m/%d'),
+                    'bookingTime': booking.bookingTime.strftime('%H:%M'),
+                    'bookingLength': booking.bookingLength.strftime('%H:%M'),
+                    'bookingType': booking.bookingType,
+                    'teamEvent': booking.teamEvent
+                    }
+
     # Validate whether any data was submitted and if it is in a valid format
 
     # The error handling was implemented and debugged
@@ -60,17 +97,6 @@ def post_booking():
 
     except KeyError:
         raise KeyError("KeyError detected")
-
-    # Create a new response
-    response = {'id': booking.id,
-                'userId': booking.userId,
-                'createDate': booking.createDate.strftime('%Y/%m/%d'),
-                'bookingDate': booking.bookingDate.strftime('%Y/%m/%d'),
-                'bookingTime': booking.bookingTime.strftime('%H:%M'),
-                'bookingLength': booking.bookingLength.strftime('%H:%M'),
-                'bookingType': booking.bookingType,
-                'teamEvent': booking.teamEvent
-                }
 
     # return the response
     return jsonify(response), 200
@@ -88,6 +114,17 @@ def delete_booking(id):
         db.session.delete(booking)
         db.session.commit()
 
+        # Create a new response
+        response = {'id': booking.id,
+                    'userId': booking.userId,
+                    'createDate': booking.createDate.strftime('%Y/%m/%d'),
+                    'bookingDate': booking.bookingDate.strftime('%Y/%m/%d'),
+                    'bookingTime': booking.bookingTime.strftime('%H:%M'),
+                    'bookingLength': booking.bookingLength.strftime('%H:%M'),
+                    'bookingType': booking.bookingType,
+                    'teamEvent': booking.teamEvent
+                    }
+
     # Validate whether any data was submitted and if it is in a valid format
     except IntegrityError:
         raise IntegrityError("IntegrityError detected", IntegrityError, None)
@@ -97,17 +134,6 @@ def delete_booking(id):
 
     except UnmappedInstanceError:
         raise UnmappedInstanceError("UnmappedInstanceError detected")
-
-    # Create a new response
-    response = {'id': booking.id,
-                'userId': booking.userId,
-                'createDate': booking.createDate.strftime('%Y/%m/%d'),
-                'bookingDate': booking.bookingDate.strftime('%Y/%m/%d'),
-                'bookingTime': booking.bookingTime.strftime('%H:%M'),
-                'bookingLength': booking.bookingLength.strftime('%H:%M'),
-                'bookingType': booking.bookingType,
-                'teamEvent': booking.teamEvent
-                }
 
     # return the response
     return jsonify(response), 200
@@ -122,6 +148,7 @@ def get_booking_uid(userId):
         bookings = models.Booking.query.filter_by(userId=userId).all()
 
         booking_list = []
+
         for booking in bookings:
             # Adding all bookings in a list called booking_list
             booking_list.append({
@@ -177,6 +204,67 @@ def get_booking_bid(id):
 
     except UnmappedInstanceError:
         raise UnmappedInstanceError("UnmappedInstanceError detected")
+
+    # return the response
+    return jsonify(response), 200
+
+
+# This function is to get a booking by using the booking id
+@app.route('/bookings/<int:id>', methods=['PATCH'])
+def patch_booking(id):
+
+    try:
+        # Requesting the data from the database by using the selected booking id
+        booking = models.Booking.query.get(id)
+
+        # Decoding the data sent to the API
+        request_data = request.get_json()
+
+        # Iterate over the key, value pairs in the request data
+        for attribute, value in request_data.items():
+
+            # Validate which attribute is being updated
+            # and execute the corresponding code
+            if attribute == "userId":
+                booking.userId = value
+            elif attribute == "createDate" or attribute == "bookingDate":
+                setattr(booking, attribute, datetime.strptime(value, '%Y/%m/%d').date())
+            elif attribute == "bookingTime" or attribute == "bookingLength":
+                setattr(booking, attribute, datetime.strptime(value, '%H:%M').time())
+            elif attribute == "bookingType":
+                booking.bookingType = value
+            elif attribute == "teamEvent":
+                booking.teamEvent = value
+
+        # Commit the changes made
+        db.session.commit()
+
+        # Create a new response
+        response = {'id': booking.id,
+                    'userId': booking.userId,
+                    'createDate': booking.createDate.strftime('%Y/%m/%d'),
+                    'bookingDate': booking.bookingDate.strftime('%Y/%m/%d'),
+                    'bookingTime': booking.bookingTime.strftime('%H:%M'),
+                    'bookingLength': booking.bookingLength.strftime('%H:%M'),
+                    'bookingType': booking.bookingType,
+                    'teamEvent': booking.teamEvent
+                    }
+
+    # Validate whether any data was submitted and if it is in a valid format
+    except IntegrityError:
+        raise IntegrityError("IntegrityError detected", IntegrityError, None)
+
+    except KeyError:
+        raise KeyError("KeyError detected")
+
+    except UnmappedInstanceError:
+        raise UnmappedInstanceError("UnmappedInstanceError detected")
+
+    except TypeError:
+        raise TypeError("TypeError detected")
+
+    except AttributeError:
+        raise AttributeError("AttributeError detected")
 
     # return the response
     return jsonify(response), 200
--- test_bookings.py
+++ test_bookings.py
@@ -2,10 +2,8 @@
 
 import os
 import pytest
-from flask import json, request, Flask
-from flask_sqlalchemy import SQLAlchemy
-from app import app, db, models
-from flask_login import current_user, login_user, logout_user
+from flask import json
+from app import app, db
 
 # The function responsible for setting up and tearing down all the data
 # and objects that may be reused in multiple tests
@@ -54,7 +52,7 @@ def test_post_valid_booking(app_fixture):
                                                 'bookingType': "General use",
                                                 'teamEvent': False})
 
-    # Validate that the correct error code and message was returned
+    # Validate that the correct status code was returned
     assert endpoint_response.status_code == 200
 
     # Decode the returned byte string
@@ -98,7 +96,7 @@ def test_post_invalid_booking(app_fixture):
     decoded_string = json.loads(endpoint_response.data)
 
     # Validate that the returned data is correct
-    assert decoded_string == {"Error": "Invalid or missing data in one or more fields"}
+    assert decoded_string == {"IntegrityError": "Invalid data in one or more fields"}
 
     # Inform that the end of this test was reached
     app.logger.info("END OF TEST: test_post_invalid_booking")
@@ -121,7 +119,7 @@ def test_post_missing_booking(app_fixture):
     decoded_string = json.loads(endpoint_response.data)
 
     # Validate that the returned data is correct
-    assert decoded_string == {"Error": "No data in the request"}
+    assert decoded_string == {"KeyError": "Missing data in the request"}
 
     # Inform that the end of this test was reached
     app.logger.info("END OF TEST: test_post_missing_booking")
@@ -146,7 +144,7 @@ def test_delete_valid_booking(app_fixture):
     # Attempt to DELETE data
     endpoint_response = app_fixture.delete("/bookings/102934536")
 
-    # Validate that the correct error code and message was returned
+    # Validate that the correct status code was returned
     assert endpoint_response.status_code == 200
 
     # Decode the returned byte string
@@ -166,7 +164,7 @@ def test_delete_valid_booking(app_fixture):
     app.logger.info("END OF TEST: test_delete_valid_booking")
 
 # DELETE a booking with invalid booking ID
-def test_delete_invalid_booking(app_fixture):
+def test_delete_missing_booking(app_fixture):
 
     # Display the message confirming this test is accessed
     app.logger.info("DELETE a booking with an invalid user ID")
@@ -194,10 +192,10 @@ def test_delete_invalid_booking(app_fixture):
     decoded_string = json.loads(endpoint_response.data)
 
     # Validate that the returned data is correct
-    assert decoded_string == {"Error": "Invalid ID in the request"}
+    assert decoded_string == {"UnmappedInstanceError": "Record not found in the database"}
 
     # Inform that the end of this test was reached
-    app.logger.info("END OF TEST: test_delete_invalid_booking")
+    app.logger.info("END OF TEST: test_delete_missing_booking")
 
 # GET all bookings of a user by a valid user ID
 def test_get_bookings_valid_uid(app_fixture):
@@ -218,7 +216,7 @@ def test_get_bookings_valid_uid(app_fixture):
 
     # POST second booking
     test_record = app_fixture.post('/booking',
-                                    json = {'id': 102934536,
+                                    json = {'id': 203948562,
                                             'userId': "21345235",
                                             'createDate': "2023/01/01",
                                             'bookingDate': "2023/01/03",
@@ -230,40 +228,250 @@ def test_get_bookings_valid_uid(app_fixture):
     # Attempt to GET the bookings
     endpoint_response = app_fixture.get("/bookings/user/21345235")
 
-    app.logger.info(endpoint_response)
+    # Validate that the correct status code was returned
+    assert endpoint_response.status_code == 200
 
+    # Decode the returned byte string
+    decoded_string = json.loads(endpoint_response.data)
 
-    # DEBUGGING NOTES:
-    # - Currently, this configuration generates and IntegrityError
-    # - Need to verify the result of the database query in views.py
+    # Validate that the returned data is correct
+    assert decoded_string == [{'id': 102934536,
+                               'userId': '21345235',
+                               'createDate': '2023/01/01',
+                               'bookingDate': '2023/01/02',
+                               'bookingTime': '13:15',
+                               'bookingLength': '01:00',
+                               'bookingType': 'General use',
+                               'teamEvent': False},
+                              {'id': 203948562,
+                               'userId': '21345235',
+                               'createDate': '2023/01/01',
+                               'bookingDate': '2023/01/03',
+                               'bookingTime': '14:30',
+                               'bookingLength': '01:15',
+                               'bookingType': 'General use',
+                               'teamEvent': False}]
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_get_bookings_valid_uid")
 
 
 # GET all bookings of a user by an invalid user ID
 def test_get_bookings_invalid_uid(app_fixture):
 
-    pass
+    # Display the message confirming this test is accessed
+    app.logger.info("GET all bookings of a user by an invalid user ID")
+
+    # POST a test booking
+    test_record = app_fixture.post('/booking',
+                                    json = {'id': 102934536,
+                                            'userId': "21345235",
+                                            'createDate': "2023/01/01",
+                                            'bookingDate': "2023/01/02",
+                                            'bookingTime': "13:15",
+                                            'bookingLength': "01:00",
+                                            'bookingType': "General use",
+                                            'teamEvent': False})
+
+    # Attempt to GET the booking by and invalid user ID
+    endpoint_response = app_fixture.get("/bookings/user/222")
+
+    # Validate that the correct status code was returned
+    assert endpoint_response.status_code == 200
+
+    # Decode the returned byte string
+    decoded_string = json.loads(endpoint_response.data)
+
+    # Validate that an empty list was returned
+    assert decoded_string == []
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_get_bookings_invalid_uid")
+
 
 # GET a specific booking by a valid booking ID
 def test_get_booking_valid_bid(app_fixture):
 
-    pass
+    # Display the message confirming this test is accessed
+    app.logger.info("GET a booking by a valid booking ID")
+
+    # POST a test booking
+    test_record = app_fixture.post('/booking',
+                                    json = {'id': 102934536,
+                                            'userId': "21345235",
+                                            'createDate': "2023/01/01",
+                                            'bookingDate': "2023/01/02",
+                                            'bookingTime': "13:15",
+                                            'bookingLength': "01:00",
+                                            'bookingType': "General use",
+                                            'teamEvent': False})
+
+    # Attempt to GET the booking
+    endpoint_response = app_fixture.get("/bookings/102934536")
+
+    # Validate that the correct status code was returned
+    assert endpoint_response.status_code == 200
+
+    # Decode the returned byte string
+    decoded_string = json.loads(endpoint_response.data)
+
+    # Validate that the returned data is correct
+    assert decoded_string == {'id': 102934536,
+                              'userId': '21345235',
+                              'createDate': '2023/01/01',
+                              'bookingDate': '2023/01/02',
+                              'bookingTime': '13:15',
+                              'bookingLength': '01:00',
+                              'bookingType': 'General use',
+                              'teamEvent': False}
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_get_booking_valid_bid")
 
 # GET a specific booking by an invalid booking ID
 def test_get_booking_invalid_bid(app_fixture):
 
-    pass
+    # Display the message confirming this test is accessed
+    app.logger.info("GET a booking by an invalid booking ID")
+
+    # POST a test booking
+    test_record = app_fixture.post('/booking',
+                                    json = {'id': 102934536,
+                                            'userId': "21345235",
+                                            'createDate': "2023/01/01",
+                                            'bookingDate': "2023/01/02",
+                                            'bookingTime': "13:15",
+                                            'bookingLength': "01:00",
+                                            'bookingType': "General use",
+                                            'teamEvent': False})
+
+    # Attempt to GET the booking by and invalid user ID
+    endpoint_response = app_fixture.get("/bookings/222")
+
+    # Validate that the correct status code was returned
+    assert endpoint_response.status_code == 400
+
+    # Decode the returned byte string
+    decoded_string = json.loads(endpoint_response.data)
+
+    # Validate that the returned data is correct
+    assert decoded_string == {"AttributeError": "The referenced record does not exist"}
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_get_booking_invalid_bid")
 
 # PATCH a booking with valid data
 def test_patch_valid_booking(app_fixture):
 
-    pass
+    # Display the message confirming this test is accessed
+    app.logger.info("PATCH a booking by a valid booking ID")
+
+    # POST a test booking
+    test_record = app_fixture.post('/booking',
+                                    json = {'id': 102934536,
+                                            'userId': "21345235",
+                                            'createDate': "2023/01/01",
+                                            'bookingDate': "2023/01/02",
+                                            'bookingTime': "13:15",
+                                            'bookingLength': "01:00",
+                                            'bookingType': "General use",
+                                            'teamEvent': False})
+
+    # Decode the returned byte string
+    decoded_string = json.loads(test_record.data)
+
+    # Display the record after POST
+    app.logger.info(f"POSTed test data: {decoded_string}")
+
+    # PATCH the booking time and length
+    update_record = app_fixture.patch('/bookings/102934536',
+                                      json = {'bookingTime': "14:15",
+                                              'bookingLength': "01:30",
+                                             })
+
+    # Decode the returned byte string
+    decoded_string = json.loads(update_record.data)
+
+    # Display the record after PATCH
+    app.logger.info(f"PATCHed test data: {decoded_string}")
+
+    # Validate that the correct status code was returned
+    assert update_record.status_code == 200
+
+    # Decode the returned byte string
+    decoded_string = json.loads(update_record.data)
+
+    # Validate that the returned data is correctly updated
+    assert decoded_string == {'id': 102934536,
+                              'userId': "21345235",
+                              'createDate': "2023/01/01",
+                              'bookingDate': "2023/01/02",
+                              'bookingTime': "14:15",
+                              'bookingLength': "01:30",
+                              'bookingType': "General use",
+                              'teamEvent': False}
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_patch_valid_booking")
 
 # PATCH a booking with invalid data
 def test_patch_invalid_booking(app_fixture):
 
-    pass
+    # Display the message confirming this test is accessed
+    app.logger.info("PATCH a booking using invalid data type in bookingTime")
+
+    # POST a test booking
+    test_record = app_fixture.post('/booking',
+                                    json = {'id': 102934536,
+                                            'userId': "21345235",
+                                            'createDate': "2023/01/01",
+                                            'bookingDate': "2023/01/02",
+                                            'bookingTime': "13:15",
+                                            'bookingLength': "01:00",
+                                            'bookingType': "General use",
+                                            'teamEvent': False})
+
+    # Decode the returned byte string
+    decoded_string = json.loads(test_record.data)
+
+    # PATCH the booking time and length using invalid data type
+    update_record = app_fixture.patch('/bookings/102934536',
+                                      json = {'bookingTime': 235234523,
+                                              'bookingLength': "01:30",
+                                             })
+
+    # Decode the returned byte string
+    decoded_string = json.loads(update_record.data)
+
+    # Validate that the correct error code and message was returned
+    assert update_record.status_code == 400
+
+    # Validate that the returned data is correct
+    assert decoded_string == {"TypeError": "Endpoint function operating on a wrong data type"}
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_patch_invalid_booking")
 
 # PATCH a booking with missing data
 def test_patch_missing_booking(app_fixture):
 
-    pass
+    # Display the message confirming this test is accessed
+    app.logger.info("PATCH a non-existent booking")
+
+    # PATCH the booking time and length of a non-existent booking
+    update_record = app_fixture.patch('/bookings/102',
+                                      json = {'bookingTime': 235234523,
+                                              'bookingLength': "01:30",
+                                             })
+
+    # Decode the returned byte string
+    decoded_string = json.loads(update_record.data)
+
+    # Validate that the correct error code and message was returned
+    assert update_record.status_code == 400
+
+    # Validate that the returned data is correct
+    assert decoded_string == {"AttributeError": "The referenced record does not exist"}
+
+    # Inform that the end of this test was reached
+    app.logger.info("END OF TEST: test_patch_missing_booking")
