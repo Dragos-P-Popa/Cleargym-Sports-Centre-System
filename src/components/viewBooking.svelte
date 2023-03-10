f01622f3107fde812479ccee948e68d46caa5995
Dragos Popa <pdragos66@gmail.com>
2023-03-10 20:47:19 +0000

    Amending booking details

--- src/components/viewBooking.svelte
+++ src/components/viewBooking.svelte
@@ -9,11 +9,63 @@
     export let bookingTime : string;
     export let bookingLength : string;
 
+    let editMode = false;
+
+    function toggleEdit() {
+        editMode = !editMode;
+    }
+
+
+  function formatDate(date : number) {
+      var d = new Date(date),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
+
+      if (month.length < 2) 
+          month = '0' + month;
+      if (day.length < 2) 
+          day = '0' + day;
+
+      return [year, month, day].join('/');
+    }
+
     async function deleteBooking(id : number){
         const res = await fetch('http://localhost:3002/bookings/'+id, {
 			method: 'DELETE'
             // enable credentials when they are implemented in the bookings API
-            //credentials: include
+            //credentials: 'include'
+        })
+    }
+
+    async function amendBooking(e: { target: HTMLFormElement; }){
+
+        // fetch form fields
+        const formData = new FormData(e.target);
+
+        const data : any = {};
+        // for each form field, create new key and assign the correct value inputted
+        // by the user
+        for (let field of formData) {
+        const [key, value] = field;
+        data[key] = value;
+        }
+
+        console.log(data);
+
+        let formattedDate = formatDate(data.date);
+        const res = await fetch('http://localhost:3002/bookings/'+bookingNumber, {
+			method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // enable credentials when they are implemented in the bookings API
+            //credentials: 'include',
+			body: JSON.stringify({
+				bookingDate: formattedDate,
+                bookingTime: data.time,
+                bookingLength: data.length
+			})
         })
     }
 </script>
@@ -40,7 +92,7 @@
     </div>
     
 
-    <form>
+    {#if editMode == false}
         <div class="py-2 flex space-x-6 px-4 text-[#1A1A1A]">
             <div class="py-2 flex-1">
                 <label for="date">Date</label> <br>
@@ -55,24 +107,52 @@
           <label for="length">Session Length</label> <br>
           <select class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" name="length" id="length" disabled>
             {#if bookingLength=="01:00"}
-            <option selected value="one">1 hour</option>
-            <option value="two">2 hours</option>
-            <option value="three">3 hours</option>
+            <option selected value="01:00">1 hour</option>
+            <option value="02:00">2 hours</option>
+            <option value="03:00">3 hours</option>
             {:else if bookingLength=="02:00"}
-            <option value="one">1 hour</option>
-            <option selected value="two">2 hours</option>
-            <option value="three">3 hours</option>
+            <option value="01:00">1 hour</option>
+            <option selected value="02:00">2 hours</option>
+            <option value="03::">3 hours</option>
             {:else}
-            <option value="one">1 hour</option>
-            <option value="two">2 hours</option>
-            <option selected value="three">3 hours</option>
+            <option value="01:00">1 hour</option>
+            <option value="02:00">2 hours</option>
+            <option selected value="03:00">3 hours</option>
             {/if}
           </select>
         </div>
-    
+
         <div class="flex justify-between  space-x-6 px-4 pt-16">
             <CancelButton on:click={() => deleteBooking(bookingNumber)} class="mt-12 w-4/5 place-self-center">Cancel booking</CancelButton>            
-            <MainButton class="mt-12 w-4/5 place-self-center">Amend booking</MainButton>
+            <MainButton on:click={() => toggleEdit()} class="mt-12 w-4/5 place-self-center">Amend booking</MainButton>
         </div>
-      </form>
+    {:else if editMode == true}
+        <form on:submit|preventDefault={amendBooking}>
+            <div class="py-2 flex space-x-6 px-4 text-[#1A1A1A]">
+                <div class="py-2 flex-1">
+                    <label for="date">Date</label> <br>
+                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="date" id="date" name="date" value="{bookingDate}"/>
+                </div>
+                <div class="py-2 flex-1">
+                    <label for="time">Time</label> <br>
+                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="time" id="time" name="time" value="{bookingTime}"/>
+                </div>
+            </div>
+            <div class="py-2 px-4 text-[#1A1A1A]">
+              <label for="length">Session Length</label> <br>
+              <select class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" name="length" id="length">
+                <option selected value="01:00">1 hour</option>
+                <option value="02:00">2 hours</option>
+                <option value="03:00">3 hours</option>
+              </select>
+            </div>
+
+        <div class="flex justify-between  space-x-6 px-4 pt-16">
+            <CancelButton on:click={() => toggleEdit()} class="mt-12 w-4/5 place-self-center">Cancel amend</CancelButton>            
+            <MainButton type="submit" class="mt-12 w-4/5 place-self-center">Confirm amend</MainButton>
+        </div>  
+        
+    </form>
+    {/if}
+
 </div>
