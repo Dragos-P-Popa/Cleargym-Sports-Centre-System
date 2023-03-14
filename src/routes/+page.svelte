7d4cb2b2db14d62974baef64eab52b8d31cd9d60
marijamaik <marijamaik@gmail.com>
2023-03-14 20:45:15 +0000

    Logo added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
 </script>
 
 <div>
-  <div class="absolute right-0 top-0 pt-4 pr-4"><SecondaryButton>Dashboard</SecondaryButton></div>
+  <img class="place-self-left pt-4 pl-6" src = "logo.svg" alt="logo"/>
+  <div class="absolute right-0 top-0 pt-4 pr-6"><SecondaryButton>Dashboard</SecondaryButton></div>
 </div>
 
 <style lang="postcss">
