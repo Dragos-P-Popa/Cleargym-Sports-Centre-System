91d0b60da4c8eaa4488eb7af30d6fc5be49fb0d7
marijamaik <marijamaik@gmail.com>
2023-03-14 23:47:11 +0000

    Footer created

---
+++ src/components/footer.svelte
@@ -0,0 +1,33 @@
+<footer class="p-10 pt-24 pb-5 bg-[#F0F9FF] text-[#106EA2] drop-shadow-md">
+
+    <div class="text-left">
+        <div class="grid-1 grid md:grid-cols-1 lg:grid-cols-6">
+            <div>
+                <img class="place-self-left py-10 pl-5" src = "logo.svg" alt="logo"/>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div class="space-y-[5px] pt-5">
+                <p class="text-[20px] pb-3"><b>Helpful</b></p>
+                <p class="text-[14px]"><a href="" class="no-underline hover:underline">FAQs</a></p>
+                <p class="text-[14px]"><a href="" class="no-underline hover:underline">Contact us</a></p>
+            </div>
+            <div class="space-y-[5px] pt-5">
+                <p class="text-[20px] pb-3"><b>Legal</b></p>
+                <p class="text-[14px]"><a href="" class="no-underline hover:underline">Terms and conditions</a></p>
+                <p class="text-[14px]"><a href="" class="no-underline hover:underline">Privacy policy</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="pt-12 text-center text-[14px]">
+      <span>Made by Squad 7</span>
+    </div>
+
+</footer>
+
+<!-- to do:
+    1. make logo bigger
+    2. make dropshadow on the top (so far only able to mae it on the bottom)
+    3. fix possitioning of objects, shouldn't use emty divs-->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,14 @@
   import  MainButton from "../components/mainButton.svelte"
   import  SecondaryButton from "../components/secondaryButton.svelte"
   import  Carousel from "../components/facilityCarousel.svelte"
+  import Footer from "../components/footer.svelte"
 </script>
 
 <div>
   <img class="place-self-left pt-4 pl-6" src = "logo.svg" alt="logo"/>
   <div class="absolute right-0 top-0 pt-4 pr-6"><SecondaryButton>Dashboard</SecondaryButton></div>
+  <!-- <div><Carousel></Carousel></div> -->
+  <div><Footer></Footer></div>
 </div>
 
 <style lang="postcss">
