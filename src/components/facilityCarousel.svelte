bc60d81f757261036fa92e92c1b52357db64abe6
marijamaik <marijamaik@gmail.com>
2023-03-15 15:39:26 +0000

    Added photos for facilities

---
+++ src/components/facilityCarousel.svelte
@@ -0,0 +1,44 @@
+<div class="relative">
+  <div class="relative">
+    <div class="relative float-left">
+      <img
+        src="swimming_pool.jpg"
+        class="block w-full"
+        alt="Swimming pool" />
+    </div>
+    <div
+      class="relative">
+      <img
+        src="fitness_room.jpg"
+        class="block w-full"
+        alt="Fitness room" />
+    </div>
+    <div
+      class="relative">
+      <img
+        src="squash_court.jpg"
+        class="block w-full"
+        alt="Squash court" />
+    </div>
+    <div class="relative float-left">
+        <img
+          src="sports_hall.jpg"
+          class="block w-full"
+          alt="Sports hall" />
+      </div>
+      <div
+        class="relative">
+        <img
+          src="climbing_wall.jpg"
+          class="block w-full"
+          alt="Climbing wall" />
+      </div>
+      <div
+        class="relative">
+        <img
+          src="studio.jpg"
+          class="block w-full"
+          alt="Studio" />
+      </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div>
   <img class="place-self-left pt-4 pl-6" src = "logo.svg" alt="logo"/>
   <div class="absolute right-0 top-0 pt-4 pr-6"><SecondaryButton>Dashboard</SecondaryButton></div>
-  <!-- <div><Carousel></Carousel></div> -->
+  <div><Carousel></Carousel></div>
   <div><Footer></Footer></div>
 </div>
 
