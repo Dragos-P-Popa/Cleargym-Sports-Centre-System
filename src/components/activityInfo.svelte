f786ceec939ebef5cb04cf9df73d5ac3d443d6f1
Yvana Qiu <yvana.qiu@gmail.com>
2023-03-15 22:27:18 +0000

    main part of the main page

---
+++ src/components/activityInfo.svelte
@@ -0,0 +1,35 @@
+<div class="flex flex-col w-full px-8">
+    <div>
+        <h2 class="px-40 my-10 text-3xl font-bold">Group Activities</h2>
+    </div>
+    <div class="flex flex-row w-full px-40 py-20">
+        <div class="flex flex-1 flex-col items-start space-y-4 px-10">
+            <h4 class="text-lg font-bold">Pilates</h4>
+            <p>
+                Welcome to our state-of-the-art sports hall, where fitness and
+                fun come together! Our spacious and fully-equipped facility is
+                designed to cater to all your fitness needs, whether you're a
+                seasoned athlete or just starting out on your fitness journey.
+            </p>
+            <img
+                src="gym.jpg"
+                class="block w-full rounded-xl"
+                alt="Swimming pool"
+            />
+        </div>
+        <div class="flex flex-1 flex-col items-start space-y-4 px-10">
+            <img
+                src="studio.jpg"
+                class="block w-full rounded-xl"
+                alt="Swimming pool"
+            />
+            <h4 class="text-lg font-bold">Yoga Hall</h4>
+            <p>
+                Welcome to our state-of-the-art sports hall, where fitness and
+                fun come together! Our spacious and fully-equipped facility is
+                designed to cater to all your fitness needs, whether you're a
+                seasoned athlete or just starting out on your fitness journey.
+            </p>
+        </div>
+    </div>
+</div>
---
+++ src/components/joinUsButton.svelte
@@ -0,0 +1,11 @@
+<script>
+    let buttonProps = {
+        class:
+            [$$restProps.class] +
+            " py-1 my-2 rounded-lg text-white font-extrabold text-xl bg-mainBlue drop-shadow-md",
+    };
+</script>
+
+<button {...buttonProps} on:click on:mouseenter on:mouseleave on:mouseover>
+    <slot />
+</button>
