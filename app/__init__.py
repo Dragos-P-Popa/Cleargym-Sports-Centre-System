7893aa7d7180488d5555687c1c6b2a85d6d7210d
Bouhamadi Mohamed Yanis <sc21myb@leeds.ac.uk>
2023-03-15 16:09:56 +0000

    fixing a minor bug in models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-#from flask_cors import CORS
+# flask-cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+import datetime
 from datetime import datetime
 
 
@@ -6,14 +7,11 @@ from datetime import datetime
 class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     userId = db.Column(db.String(50), nullable=False)
-    facilityId = db.Column(db.Integer, nullable = False)
+    facilityId = db.Column(db.Integer, nullable=False)
     activityId = db.Column(db.Integer, nullable=False)
-    createDate = db.Column(db.Date, nullable=False, default=datetime.datetime.now())
+    createDate = db.Column(db.Date, nullable=False, default=datetime.now())
     bookingDate = db.Column(db.Date, nullable=False)
     bookingTime = db.Column(db.Time, nullable=False)
     bookingLength = db.Column(db.Time, nullable=False)
     bookingEndTime = db.Column(db.Time, nullable=False)
     bookingType = db.Column(db.String(100), nullable=False)
-
-
-
