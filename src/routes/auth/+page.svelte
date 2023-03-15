a27095c26818a0174a3815037b431e1df04302a0
Dragos Popa <pdragos66@gmail.com>
2023-03-15 11:52:49 +0000

    UI improvements for auth page
    Added a few fixes for the auth page. The background is blurred to match
    the figma design. Also the background image resizes to match screen
    size.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -269,86 +269,87 @@
   }
 </script>
 
-<body class="min-h-screen">
-<div class="grid rounded-xl bg-white shadow-lg border-[1px] border-borderColor m-8 mx-[30%] max-w-[40%] px-16 pt-12 pb-8">
-  <img class="place-self-center pr-3" src = "logo.svg" alt="logo"/>
-  <p class="text-center text-5xl font-bold pt-4">Welcome to cleargym!</p>
-  <p class="text-center text-xl font-light pb-20">Lorem ipsum lorem ipsum lerem ipsum</p>
+<div class="min-h-screen h-fill myclass">
+  <div class="w-screen h-fill backdrop-blur-sm pb-12 pt-12">
+    <div class="grid rounded-xl bg-white shadow-lg border-[1px] border-borderColor m-8 mx-[30%] max-w-[40%] px-16 pt-12 pb-8">
+      <img class="place-self-center pr-3" src = "logo.svg" alt="logo"/>
+      <p class="text-center text-5xl font-bold pt-4">Welcome to cleargym!</p>
+      <p class="text-center text-xl font-light pb-20">Lorem ipsum lorem ipsum lerem ipsum</p>
 
-  <!--currently in login UI-->
-  {#if registerToggle == false}
-  <form on:submit|preventDefault={loginSubmit}>
-    <div>
-      <label for="email">Email</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="email" name="email" placeholder="example@email.com" value="" />
-      <p id="emailError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
-    <div class="mt-4 pb-8">
-      <label for="password">Password</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="password" id="password" placeholder="password" name="password" value="" />
-      <p id="passwordError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
+      <!--currently in login UI-->
+      {#if registerToggle == false}
+      <form on:submit|preventDefault={loginSubmit}>
+        <div>
+          <label for="email">Email</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="email" name="email" placeholder="example@email.com" value="" />
+          <p id="emailError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
+        <div class="mt-4 pb-8">
+          <label for="password">Password</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="password" id="password" placeholder="password" name="password" value="" />
+          <p id="passwordError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
 
-    <p class="p-3 rounded-md bg-[#f43f5f99] font-semibold" id="serverError" style="display: none;"></p>
+        <p class="p-3 rounded-md bg-[#f43f5f99] font-semibold" id="serverError" style="display: none;"></p>
 
-    <div class="grid">
-      <MainButton type="submit" class="mt-12  w-3/5 max-w-lg place-self-center">Login</MainButton>
-    </div>
-  </form>
+        <div class="grid">
+          <MainButton type="submit" class="mt-12  w-3/5 max-w-lg place-self-center">Login</MainButton>
+        </div>
+      </form>
 
-  <div class="grid">
-    <div class="inline-flex items-center justify-center w-full">
-      <hr class="w-64 h-px my-4 bg-gray-200 border-0">
-      <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2">or</span>
+      <div class="grid">
+        <div class="inline-flex items-center justify-center w-full">
+          <hr class="w-64 h-px my-4 bg-gray-200 border-0">
+          <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2">or</span>
+        </div>
+        <!--switch to register UI (line 151 - 177)-->
+        <SecondaryButton on:click={() => toggle()} class="place-self-center  w-3/5 max-w-lg ">Register</SecondaryButton>
+      </div>
+      {:else}
+      <form on:submit|preventDefault={registerSubmit}>
+        <div class="py-2">
+          <label for="firstName">First Name</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="firstName" placeholder="John" name="firstName" value="" />
+          <p id="fNameError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
+        <div class="py-2">
+          <label for="lastName">Last Name</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="lastName" placeholder="Doe" name="lastName" value="" />
+          <p id="lNameError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
+        <div class="py-2">
+          <label for="email">Email</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="email" name="email" placeholder="example@email.com" value="" />
+          <p id="emailError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
+        <div class="py-2">
+          <label for="password">Password</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="password" id="password" placeholder="password" name="password" value="" />
+          <p id="passwordError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
+        <div class="py-2">
+          <label for="rPassword">Repeat Password</label> <br>
+          <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="password" id="rPassword" placeholder="password" name="rPassword" value="" />
+          <p id="rPasswordError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
+        </div>
+
+        <p class="p-3 rounded-md bg-[#f43f5f99] font-semibold" id="serverError" style="display: none;"></p>
+
+        <div class="grid">
+          <MainButton type="submit" class="mt-12  w-3/5 max-w-lg place-self-center">Register</MainButton>
+        </div>
+      </form>
+      {/if}
+      
+
     </div>
-    <!--switch to register UI (line 151 - 177)-->
-    <SecondaryButton on:click={() => toggle()} class="place-self-center  w-3/5 max-w-lg ">Register</SecondaryButton>
   </div>
-  {:else}
-  <form on:submit|preventDefault={registerSubmit}>
-    <div class="py-2">
-      <label for="firstName">First Name</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="firstName" placeholder="John" name="firstName" value="" />
-      <p id="fNameError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
-    <div class="py-2">
-      <label for="lastName">Last Name</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="lastName" placeholder="Doe" name="lastName" value="" />
-      <p id="lNameError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
-    <div class="py-2">
-      <label for="email">Email</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="text" id="email" name="email" placeholder="example@email.com" value="" />
-      <p id="emailError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
-    <div class="py-2">
-      <label for="password">Password</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="password" id="password" placeholder="password" name="password" value="" />
-      <p id="passwordError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
-    <div class="py-2">
-      <label for="rPassword">Repeat Password</label> <br>
-      <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="password" id="rPassword" placeholder="password" name="rPassword" value="" />
-      <p id="rPasswordError" class="ml-2 mt-1 text-[#f43f5f] text-sm"></p>
-    </div>
-
-    <p class="p-3 rounded-md bg-[#f43f5f99] font-semibold" id="serverError" style="display: none;"></p>
-
-    <div class="grid">
-      <MainButton type="submit" class="mt-12  w-3/5 max-w-lg place-self-center">Register</MainButton>
-    </div>
-  </form>
-  {/if}
-  
-
 </div>
-</body>
 
 <style>
-  body {
+  .myclass {
     background-image: url("gym.jpg");
     background-size: cover;
-    padding-top: 5rem;
     font-family: "Manrope";
     }
 
