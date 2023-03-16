5fac17037285cd89c044e27e89c8d997053b951d
Sinay27 <yanis.bouhamadi2020@gmail.com>
2023-03-16 14:07:11 +0100

    Developing the availability class

--- availability.py
+++ availability.py
@@ -5,16 +5,27 @@ from app import app, db, models
 
 
 class Booking:
-    def __init__(self, Bdate, Btime, Blength, Bend, Btype):
+    def __init__(self, Bdate, Btime, Blength, Bend, Btype, FacilityId, Close, Open, Capacity):
         self.Bdate = Bdate
         self.Btime = Btime
         self.Blength = Blength
         self.Bend = Bend
         self.Btype = Btype
+        self.FacilityId = FacilityId
+        self.Open = Open
+        self.Close = Close
+        self.Capacity = Capacity
 
-     # length_one is a function that accepts or refuses a
+    def check_time(self, Btime, Bend, Open, Close):
+
+        if Close > Btime > Open and Close > Bend > Open:
+            return True
+        else:
+            abort(400, description='Too Early / Late')
+
+    # length_one is a function that accepts or refuses a
     # booking that has 1 hour length.
-    def length_one(self, booking, Bdate, Btime, Blength, Bend, Btype):
+    def length_one(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity):
 
         start = datetime.combine(datetime.today(), Btime)
         end = datetime.combine(datetime.today(), Bend)
@@ -28,20 +39,20 @@ class Booking:
                       + timedelta(hours=twohours.hour, minutes=twohours.minute, seconds=twohours.second)).time()
 
         # All Bookings that starts at booking start time.
-        a1 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=Btime,
-                                                bookingType=Btype).all()
+        a1 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=Btime,
+                                            bookingType=Btype).all()
 
         # All Bookings that starts booking start time and ends in one hour
-        a2 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=Btime,
-                                                bookingEndTime=moins_deux,
-                                                bookingType=Btype).all()
+        a2 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=Btime,
+                                            bookingEndTime=moins_deux,
+                                            bookingType=Btype).all()
 
         # All bookings that ends after 1 hour of booking start time
-        a3 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingEndTime=moins_deux,
-                                                bookingType=Btype).all()
+        a3 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingEndTime=moins_deux,
+                                            bookingType=Btype).all()
         b1 = len(a1)
         b2 = len(a2)
         b3 = len(a3)
@@ -49,7 +60,7 @@ class Booking:
         Block1 = (b1 + b2) - b3
         Block2 = Block3 = 0
 
-        if Block1 >= 3 and Btype == "Swimming pool":
+        if Block1 >= Capacity:
             abort(400, description='Invalid time Block 1')
         else:
             return True
@@ -57,7 +68,7 @@ class Booking:
         return Block1, Block2, Block3
 
     # length_two is a function that accepts or refuses a booking that has 2 hours length
-    def length_two(self, booking, Bdate, Btime, Blength, Bend, Btype):
+    def length_two(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity):
 
         start = datetime.combine(datetime.today(), Btime)
         end = datetime.combine(datetime.today(), Bend)
@@ -71,32 +82,32 @@ class Booking:
                       + timedelta(hours=twohours.hour, minutes=twohours.minute, seconds=twohours.second)).time()
 
         # all booking that ends at end time
-        a = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                               bookingEndTime=Bend,
-                                               bookingType=booking.bookingType).all()
+        a = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                           bookingEndTime=Bend,
+                                           bookingType=booking.bookingType).all()
         # all booking that ends 1 hour  before the end time
-        b = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                               bookingEndTime=moins_un,
-                                               bookingType=Btype).all()
+        b = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                           bookingEndTime=moins_un,
+                                           bookingType=Btype).all()
         # all bookings that starts 1 hour before end time
-        c = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                               bookingTime=moins_un,
-                                               bookingType=Btype).all()
+        c = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                           bookingTime=moins_un,
+                                           bookingType=Btype).all()
         # All Bookings that starts at booking start time.
-        d = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                               bookingTime=Btime,
-                                               bookingType=Btype).all()
+        d = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                           bookingTime=Btime,
+                                           bookingType=Btype).all()
 
         # All Bookings that starts booking start time and ends in one hour
-        e = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                               bookingTime=Btime,
-                                               bookingEndTime=moins_un,
-                                               bookingType=Btype).all()
+        e = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                           bookingTime=Btime,
+                                           bookingEndTime=moins_un,
+                                           bookingType=Btype).all()
         # All Bookings that starts at New booking start time + 1 Hour and ends at New booking ends time
-        f = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                               bookingTime=moins_un,
-                                               bookingEndTime=Bend,
-                                               bookingType=Btype).all()
+        f = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                           bookingTime=moins_un,
+                                           bookingEndTime=Bend,
+                                           bookingType=Btype).all()
 
         aa = len(a)
         bb = len(b)
@@ -109,17 +120,17 @@ class Booking:
         Block1 = (dd + bb) - ee
         Block3 = 0
 
-        if Block1 >= 3 and Btype == "Swimming pool":
-            abort(400, description='Invalid time Block 1')
-        elif Block2 >= 3 and Btype == "Swimming pool":
-            abort(400, description='Invalid time Block 2')
+        if Block1 >= Capacity:
+            abort(400, description='Full Capacity in the first part of your Booking')
+        elif Block2 >= Capacity:
+            abort(400, description='Full Capacity in the second part of your Booking')
         else:
             return True
 
         return Block1, Block2, Block3
 
     # length_one is a function that accepts or refuses a booking that has 3 hours length
-    def length_three(self, booking, Bdate, Btime, Blength, Bend, Btype):
+    def length_three(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity):
 
         start = datetime.combine(datetime.today(), Btime)
         end = datetime.combine(datetime.today(), Bend)
@@ -134,51 +145,51 @@ class Booking:
 
         # Block 1
         # All Bookings that starts at booking start time.
-        a1 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=Btime,
-                                                bookingType=Btype).all()
+        a1 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=Btime,
+                                            bookingType=Btype).all()
 
         # All Bookings that starts booking start time and ends in one hour
-        a2 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=Btime,
-                                                bookingEndTime=moins_deux,
-                                                bookingType=Btype).all()
+        a2 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=Btime,
+                                            bookingEndTime=moins_deux,
+                                            bookingType=Btype).all()
 
         # All bookings that ends after 1 hour of booking start time
-        a3 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingEndTime=moins_deux,
-                                                bookingType=Btype).all()
+        a3 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingEndTime=moins_deux,
+                                            bookingType=Btype).all()
         # Block 2
         # All bookings that starts 1 hour after booking start time
-        a4 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=moins_deux,
-                                                bookingType=Btype).all()
+        a4 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=moins_deux,
+                                            bookingType=Btype).all()
 
         # All booking that ends 1 hour before the end time
-        a5 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingEndTime=moins_un,
-                                                bookingType=Btype).all()
+        a5 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingEndTime=moins_un,
+                                            bookingType=Btype).all()
 
-        a6 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=moins_un,
-                                                bookingEndTime=moins_un,
-                                                bookingType=Btype).all()
+        a6 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=moins_un,
+                                            bookingEndTime=moins_un,
+                                            bookingType=Btype).all()
         # Block 3
         # All booking that starts after 2 hours
-        a7 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=moins_un,
-                                                bookingType=Btype).all()
+        a7 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=moins_un,
+                                            bookingType=Btype).all()
 
         # All booking that ends at Booking end time
-        a8 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingEndTime=Bend,
-                                                bookingType=Btype).all()
+        a8 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingEndTime=Bend,
+                                            bookingType=Btype).all()
 
         # All booking that starts after 2 hours and ends at Booking end time
-        a9 = models.Reservation.query.filter_by(bookingDate=booking.bookingDate,
-                                                bookingTime=moins_un,
-                                                bookingEndTime=Bend,
-                                                bookingType=Btype).all()
+        a9 = models.Booking.query.filter_by(bookingDate=booking.bookingDate,
+                                            bookingTime=moins_un,
+                                            bookingEndTime=Bend,
+                                            bookingType=Btype).all()
 
         b1 = len(a1)
         b2 = len(a2)
@@ -193,55 +204,27 @@ class Booking:
         Block1 = (b1 + b2) - b3
         Block2 = (b4 + b5) - b6
         Block3 = (b7 + b8) - b9
-        print(Block1, Block2, Block3)
 
-        if Block1 >= 3 and Btype == "Swimming pool":
-            abort(400, description='Invalid time Block 1')
-        elif Block2 >= 3 and Btype == "Swimming pool":
-            abort(400, description='Invalid time Block 2')
-        elif Block3 >= 3 and Btype == "Swimming pool":
-            abort(400, description='Invalid time Block 3')
+        if Block1 >= Capacity:
+            abort(400, description='Full Capacity in the first part of your Booking')
+        elif Block2 >= Capacity:
+            abort(400, description='Full Capacity in the second part of your Booking')
+        elif Block3 >= Capacity:
+            abort(400, description='Full Capacity in the third part of your Booking')
         else:
             return True
         return Block1, Block2, Block3
 
-    def check_activity(self, Btype):
+    def check_length(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity):
+        onehour = time(1, 0, 0)
+        twohours = time(2, 0, 0)
+        threehours = time(3, 0, 0)
 
-        if self.Btype == "Swimming pool":
-            return True
-        elif self.Btype == "Fitness room":
-            return True
-        elif self.Btype == "Sports hall":
-            return True
-        elif self.Btype == "Studio":
-            return True
-        elif self.Btype == "Climbing wall":
-            return True
+        if Blength == onehour:
+            Booking.length_one(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity)
+        elif Blength == twohours:
+            Booking.length_two(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity)
+        elif Blength == threehours:
+            Booking.length_three(self, booking, Bdate, Btime, Blength, Bend, Btype, Capacity)
         else:
-            abort(400, description='Invalid Facility')
-
-    def check_time(self, Btime, Blength, Btype):
-
-        openning_time = time(8, 0, 0)
-        ten_am = time(10, 0, 0)
-        eight_pm = time(20, 0, 0)
-        closing_time = time(22, 0, 0)
-        booking_ending_time = (datetime.combine(datetime.today(), Btime)
-                               + timedelta(hours=Blength.hour, minutes=Blength.minute, seconds=Blength.second)).time()
-
-        if self.Btype == "Swimming pool" and eight_pm > Btime > openning_time and eight_pm > booking_ending_time > openning_time:
-            return True
-        elif self.Btype == "Fitness room" and closing_time > Btime > openning_time and closing_time > booking_ending_time > openning_time:
-            return True
-        elif self.Btype == "Sports hall" and closing_time > Btime > openning_time and closing_time > booking_ending_time > openning_time:
-            return True
-        elif self.Btype == "Studio" and closing_time > Btime > openning_time and closing_time > booking_ending_time > openning_time:
-            return True
-        elif self.Btype == "Climbing wall" and eight_pm > Btime > ten_am and eight_pm > booking_ending_time > ten_am:
-            return True
-        else:
-            abort(400, description='Invalid time')
-
-    # def check_availability(self, booking, Bdate, Btime, Blength, Btype):
-
-    # time_plus_one = (datetime.combine(datetime.today(), Btime) + timedelta(hours=Hone.hour, minutes=Hone.minute,seconds=Hone.second)).time()
+            abort(400, description='Invalid Length')
