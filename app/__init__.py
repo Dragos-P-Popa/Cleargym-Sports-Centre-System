c41739dbcd48c22e33f8078eb4646b0d3f15881f
Bouhamadi Mohamed Yanis <sc21myb@leeds.ac.uk>
2023-03-16 12:13:40 +0000

    Updating init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask-cors import  CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
