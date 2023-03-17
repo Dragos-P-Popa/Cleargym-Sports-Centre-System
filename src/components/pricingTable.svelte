1ac0ae9a826256b89e6ae228fe121993e595b711
Ali Almoharif <51306868+ASHS21@users.noreply.github.com>
2023-03-17 15:08:40 +0000

    membership cards and pricing table implemented

---
+++ src/components/pricingTable.svelte
@@ -0,0 +1,142 @@
+
+
+    <table class="w-full border-borderColor border mt-4 table-auto border-spacing-6 border-collapse">
+        <thead>
+        <tr>
+            <th class=" border-borderColor border p-2">
+                Facility
+            </th>
+            <th class=" border-borderColor border p-2">
+                Activity
+            </th>
+            <th class=" border-borderColor border p-2">
+                1 hour
+            </th>
+            <th class=" border-borderColor border p-2">
+                2-3 hours
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class=" border-borderColor border p-4">Swimming pool</td>
+            <td class=" border-borderColor border p-2">
+                <ul>
+                    <li class=" p-2">
+                        General use
+                    </li>
+                    <li class=" p-2">
+                        Lane swimming
+                    </li>
+                    <li class=" p-2">
+                        Lessons
+                    </li>
+                    <li class=" p-2">
+                        Team event
+                    </li>
+                </ul>
+            </td>
+            <td class=" border-borderColor border p-2">
+                <ul>
+                    <li class="p-2">
+                        £10
+                    </li>
+                    <li class=" p-2">
+                        £10
+                    </li>
+                    <li class=" p-2">
+                        £10
+                    </li>
+                    <li class=" p-2">
+                        Not applicable
+                    </li>
+                </ul>
+            </td>
+            <td class=" border-borderColor border p-2">
+                <ul>
+                    <li class="  p-2">
+                        £8 ph
+                    </li>
+                    <li class=" p-2">
+                        £8 ph
+                    </li>
+                    <li class="p-2">
+                        £8 ph
+                    </li>
+                    <li class="p-2">
+                        Contact our sales team
+                    </li>
+                </ul>
+            </td>
+
+        </tr>
+        <tr>
+            <td class=" border-borderColor border p-4">Fitness room</td>
+            <td class=" border-borderColor border p-2"> General use</td>
+            <td class=" border-borderColor border p-2">£10</td>
+            <td class=" border-borderColor border p-2">£8 ph</td>
+
+        </tr>
+        <tr>
+            <td class=" border-borderColor border p-4">Squash court</td>
+            <td class=" border-borderColor border p-2">1-hour session</td>
+            <td class=" border-borderColor border p-2">£10</td>
+            <td class=" border-borderColor border p-2">Not applicable</td>
+
+        </tr>
+        <tr>
+            <td class=" border-borderColor border p-4">Sport hall</td>
+            <td class=" border-borderColor border p-2">
+                <ul>
+                    <li class="p-2">
+                        1-hour session
+                    </li>
+                    <li class=" p-2">
+                        Team event
+                    </li>
+                </ul>
+            </td>
+            <td class=" border-borderColor border p-2">
+                <ul>
+                    <li class="p-2">
+                       £10
+                    </li>
+                    <li class=" p-2">
+                        Not applicable
+                    </li>
+                </ul>
+            </td>
+            <td class=" border-borderColor border p-2">
+                <ul>
+                    <li class="p-2">
+                        £8 ph
+                    </li>
+                    <li class=" p-2">
+                        Contact our sales team
+                    </li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td class=" border-borderColor border p-4">Climbing wall</td>
+            <td class=" border-borderColor border p-2"> General use</td>
+            <td class=" border-borderColor border p-2">£10</td>
+            <td class=" border-borderColor border p-2">£8 ph</td>
+
+        </tr>
+        <tr>
+            <td class=" border-borderColor border p-4">Studio</td>
+            <td class=" border-borderColor border p-2">Exercise classes</td>
+            <td class=" border-borderColor border p-2">£10</td>
+            <td class=" border-borderColor border p-2">£8 ph</td>
+
+        </tr>
+        </tbody>
+
+    </table>
+
+
+
+
+
+
