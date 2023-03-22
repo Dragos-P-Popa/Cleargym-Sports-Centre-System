e184f2f72a70cc85df6981401ea6d722eef6aa7a
Dragos Popa <pdragos66@gmail.com>
2023-03-22 10:04:36 +0000

    Carousel bugfix

--- src/components/homeCarousel.svelte
+++ src/components/homeCarousel.svelte
@@ -1,33 +1,37 @@
 <!-- Carousel for Home page -->
 <script lang="ts">
     import { onMount } from 'svelte';
-    let container: HTMLElement;
 
     onMount(() => {
-        const arrowLeft = document.getElementById('arrow-left');
-        const arrowRight = document.getElementById('arrow-right');
+        const arrowLeft = document.getElementById('arrow-left')!;
+        const arrowRight = document.getElementById('arrow-right')!;
+        const img = document.getElementById('showcase')!;
+
+        let images = ['swimming_pool_c.webp', 'fitness_room_c.webp', 'squash_court_c.webp', 'sports_hall_c.webp', 'climbing_wall_c.webp', 'studio_c.webp']
+        let i = 0
         
         // swipe left when left arrow pressed
         arrowLeft.addEventListener('click', () => {
-        container.scrollLeft -= 720;
+            if (i > 0){
+                i = i - 1
+                img.src = images[i]
+            }
         });
         
         // swipe right when right arrow pressed
         arrowRight.addEventListener('click', () => {
-        container.scrollLeft += 720;
+            if (i < images.length-1){
+                i = i + 1
+                img.src = images[i]
+            }
         });
     });
 
 </script>
   
 <div class="pb-10 pt-10 px-8 relative">
-    <div class="flex overflow-x-auto no-scroll rounded-lg drop-shadow-lg" bind:this={container}>
-        <img src="swimming_pool_c.jpg" class="block w-full" alt="Swimming pool" />
-        <img src="fitness_room_c.jpg" class="block w-full" alt="Fitness room" />
-        <img src="squash_court_c.jpg" class="block w-full" alt="Squash court" />
-        <img src="sports_hall_c.jpg" class="block w-full" alt="Sports hall" />
-        <img src="climbing_wall_c.jpg" class="block w-full" alt="Climbing wall" />
-        <img src="studio_c.jpg" class="block w-full" alt="Studio" />
+    <div class="flex overflow-x-auto no-scroll rounded-lg drop-shadow-lg">
+        <img id="showcase" src="swimming_pool_c.webp" class="block w-full" alt="showcase" />
     </div>
     <div class="absolute pl-2 text-white text-2xl top-2/4 pt-24 left-28 font-semibold">New classes available</div>
     <div class="absolute pl-2 text-[#D7D7D7] text-base top-3/4 left-28">Book one of our new classes now!</div>
