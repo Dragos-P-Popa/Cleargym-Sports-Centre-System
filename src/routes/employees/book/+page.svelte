d2c6ec9797d1036cce2eeaebd94705c70f2c28c2
Yvana Qiu <yvana.qiu@gmail.com>
2023-03-26 22:21:28 +0100

    combined components

--- src/routes/employees/book/+page.svelte
+++ src/routes/employees/book/+page.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
   import "@fontsource/manrope";
+  import EmployeeBooking from "../../../components/employeeBooking.svelte";
   import NavBar from "../../../components/employeeNavbar.svelte";
+  import QuickBooking from "../../../components/quickBooking.svelte";
 
   export let data;
   let user = data.user;
 </script>
 
 <div class="grid grid-cols-12">
-  <NavBar active=0 firstName={user.firstName} lastName={user.lastName}/>
+  <NavBar active="0" firstName={user.firstName} lastName={user.lastName} />
 
   <div class="col-span-10 pt-12 px-8">
-    
+    <p class="font-bold text-5xl text-[#1A1A1A] w-full pb-10">
+      Create a Booking
+    </p>
+    <EmployeeBooking />
   </div>
 </div>
 
