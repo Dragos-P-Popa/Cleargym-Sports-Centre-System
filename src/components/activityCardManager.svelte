488b919e21074a1d09bc9cda59cc5ac378145756
marijamaik <marijamaik@gmail.com>
2023-03-29 00:23:43 +0100

    Edit mode displays correct information about activity

--- src/components/activityCardManager.svelte
+++ src/components/activityCardManager.svelte
@@ -15,8 +15,6 @@
     
     let editMode : boolean = false;
     let facilities : any = {};
-
-
   
     async function editActivity(e: { target: HTMLFormElement; }) {
       // fetch form fields
@@ -97,43 +95,44 @@
                 <label for="time">Name</label> <br>
                 <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="name" id="name" name="name" value="{activityType}" />
             </div>
-            <div class="py-2">
-                <label for="day">Facility</label> <br>
-                <select class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" name="length" id="length">
-                {#await facilityLoading()}
-                <option value="loading">Loading...</option>
-                {:then facilities}
-                    {#each facilities as facility, i}
-                    <option value={facility.facilitiesId}>{facility.facilityName}</option>
-                    {/each}
-                {/await}
-                </select>
+            <div class="flex space-x-6 text-[#1A1A1A]">
+                <div class="py-2 flex-1">
+                    <label for="day">Facility</label> <br>
+                    <select class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" name="length" id="length">
+                    {#await facilityLoading()}
+                    <option value="loading">Loading...</option>
+                    {:then facilities}
+                        {#each facilities as facility, i}
+                        <option value={facility.facilitiesId}>{facility.facilityName}</option>
+                        {/each}
+                    {/await}
+                    </select>
+                </div>
+                <div class="py-2 flex-1">
+                    <label for="time">Activity Day</label> <br>
+                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="name" id="name" name="name" value="{activityDay}" />
+                </div>
             </div>
+            
 
             <div class="flex space-x-6 text-[#1A1A1A]">
                 <div class="py-2 flex-1">
-                    <label for="time">Time</label> <br>
-                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="time" id="time" name="time" value="" />
+                    <label for="time">Start Time</label> <br>
+                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="time" id="time" name="time" value="{activityStartTime}" />
                 </div>
                 <div class="py-2 flex-1">
-                    <label for="length">Length</label> <br>
-                    <select class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" name="length" id="length">
-                    <option value="01:00">1 hour</option>
-                    <option value="02:00">2 hours</option>
-                    <option value="03:00">3 hours</option>
-                    </select>
+                    <label for="time">End Time</label> <br>
+                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="time" id="time" name="time" value="{activityEndTime}" />
                 </div>
             </div>
-            <div class="flex space-x-6 text-[#1A1A1A]">
-                <div class="py-2 flex-1">
-                    <label for="time">Price £</label> <br>
-                    <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="number" id="price" name="price" value="0" min="0" />
-                </div>
+            <div class="py-2">
+                <label for="time">Price £</label> <br>
+                <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="number" id="price" name="price" value="{activityPrice}" min="0" />
+            </div>
                 <!-- <div class="py-2 flex-1">
                     <label for="time">Discount %</label> <br>
                     <input class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-full" type="number" id="discount" name="discount" value="0" min="0" />
                 </div> -->
-            </div>
             <div class="flex space-x-6 pr-3">
                 <CancelButton on:click={() => editMode = false} class="mt-5 mx-2 flex-1">Cancel</CancelButton>
                 <MainButton on:click={()=>editMode = false} type="submit" class="mt-5 mx-2 flex-1">Save</MainButton>          
