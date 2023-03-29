3defeaab4e27feaff6f612406b0f9ee02d51d296
Ali Almoharif <51306868+ASHS21@users.noreply.github.com>
2023-03-29 14:39:33 +0100

    pricing table changed

--- src/components/pricingTable.svelte
+++ src/components/pricingTable.svelte
@@ -1,3 +1,38 @@
+<script>
+        import { PUBLIC_FACILITIES_URL } from '$env/static/public'
+let facilities;
+let activities; 
+
+     async function facilityLoading() {
+    // fetch all facilities
+		const res = await fetch(PUBLIC_FACILITIES_URL + 'facilities', {
+			method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+		})
+
+    
+	  facilities = await res.json()
+    return facilities
+	}
+
+    async function activityLoading() {
+    // fetch all activites
+		const res = await fetch(PUBLIC_FACILITIES_URL + 'activities', {
+			method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+      }
+		})
+
+    activities = await res.json()
+    return activities
+	}
+</script>
+
+
+
 
 
     <table class="w-full border-borderColor border mt-4 table-auto border-spacing-6 border-collapse">
@@ -12,127 +47,43 @@
             <th class=" border-borderColor border p-2">
                 1 hour
             </th>
-            <th class=" border-borderColor border p-2">
-                2-3 hours
-            </th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td class=" border-borderColor border p-4">Swimming pool</td>
-            <td class=" border-borderColor border p-2">
+        
+            {#await facilityLoading()}
+            {:then facilities}
+            {#each facilities as facility}
+
+            <tr>
+            <td class=" border-borderColor border p-4">{facility.facilityName}</td>
+            
+            <td class=" border-borderColor border p-4">
+                {#await activityLoading()}
+                {:then activities}
+                {#each activities as activity}             
                 <ul>
-                    <li class=" p-2">
-                        General use
-                    </li>
-                    <li class=" p-2">
-                        Lane swimming
-                    </li>
-                    <li class=" p-2">
-                        Lessons
-                    </li>
-                    <li class=" p-2">
-                        Team event
+                    <li class="p-2">
+                        {activity.activityType}
+                        
                     </li>
                 </ul>
+                {/each}
+                {/await}
             </td>
-            <td class=" border-borderColor border p-2">
-                <ul>
-                    <li class="p-2">
-                        £10
-                    </li>
-                    <li class=" p-2">
-                        £10
-                    </li>
-                    <li class=" p-2">
-                        £10
-                    </li>
-                    <li class=" p-2">
-                        Not applicable
-                    </li>
-                </ul>
-            </td>
-            <td class=" border-borderColor border p-2">
-                <ul>
-                    <li class="  p-2">
-                        £8 ph
-                    </li>
-                    <li class=" p-2">
-                        £8 ph
-                    </li>
-                    <li class="p-2">
-                        £8 ph
-                    </li>
-                    <li class="p-2">
-                        Contact our sales team
-                    </li>
-                </ul>
+            <td class=" border-borderColor border p-4">
+
             </td>
 
-        </tr>
-        <tr>
-            <td class=" border-borderColor border p-4">Fitness room</td>
-            <td class=" border-borderColor border p-2"> General use</td>
-            <td class=" border-borderColor border p-2">£10</td>
-            <td class=" border-borderColor border p-2">£8 ph</td>
 
-        </tr>
-        <tr>
-            <td class=" border-borderColor border p-4">Squash court</td>
-            <td class=" border-borderColor border p-2">1-hour session</td>
-            <td class=" border-borderColor border p-2">£10</td>
-            <td class=" border-borderColor border p-2">Not applicable</td>
+            </tr>    
+            {/each}
+            {/await}
+            
 
-        </tr>
-        <tr>
-            <td class=" border-borderColor border p-4">Sport hall</td>
-            <td class=" border-borderColor border p-2">
-                <ul>
-                    <li class="p-2">
-                        1-hour session
-                    </li>
-                    <li class=" p-2">
-                        Team event
-                    </li>
-                </ul>
-            </td>
-            <td class=" border-borderColor border p-2">
-                <ul>
-                    <li class="p-2">
-                       £10
-                    </li>
-                    <li class=" p-2">
-                        Not applicable
-                    </li>
-                </ul>
-            </td>
-            <td class=" border-borderColor border p-2">
-                <ul>
-                    <li class="p-2">
-                        £8 ph
-                    </li>
-                    <li class=" p-2">
-                        Contact our sales team
-                    </li>
-                </ul>
-            </td>
-        </tr>
-        <tr>
-            <td class=" border-borderColor border p-4">Climbing wall</td>
-            <td class=" border-borderColor border p-2"> General use</td>
-            <td class=" border-borderColor border p-2">£10</td>
-            <td class=" border-borderColor border p-2">£8 ph</td>
+        
 
-        </tr>
-        <tr>
-            <td class=" border-borderColor border p-4">Studio</td>
-            <td class=" border-borderColor border p-2">Exercise classes</td>
-            <td class=" border-borderColor border p-2">£10</td>
-            <td class=" border-borderColor border p-2">£8 ph</td>
-
-        </tr>
         </tbody>
-
     </table>
 
 
