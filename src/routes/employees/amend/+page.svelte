22196808880954c0b3ac787f1b1b372e9f610e8b
Yvana Qiu <yvana.qiu@gmail.com>
2023-03-31 03:15:53 +0100

    merged more components, i.e: calendar

--- src/routes/employees/amend/+page.svelte
+++ src/routes/employees/amend/+page.svelte
@@ -19,60 +19,6 @@
   <div class="col-span-10 pt-12 px-8">
     <p class="font-bold text-5xl text-[#1A1A1A] w-full pb-10">Amendments</p>
     <SearchEmail />
-    <!--  <div class="w-5/6">
-        <label for="customerEmail">Customer Email</label>
-        <input
-          class="border-borderColor border-[1px] rounded-md px-2 py-2 mt-1 shadow-sm min-w-ful w-full"
-          type="text"
-          id="customerEmail"
-          name="customerEmail"
-        />
-      </div>
-      <div>
-        <JoinUsButton class="items-end m-5 w-3/4 mt-7 h-10">Search</JoinUsButton
-        >
-      </div> -->
-    <!--     <div class="flex flex-row w-full mb-10">
-      <ClientCard class="w-5/6" />
-      <div class="flex flex-col">
-        <MainButton class="m-8 w-5/6">Bookings</MainButton>
-        <MainButton class="m-8 w-5/6">Membership</MainButton>
-      </div>
-    </div> -->
-
-    <!--     <div class="flex flex-row">
-      <div class="flex flex-col w-1/2 px-1">
-        <BookingCard class="w-4/5" />
-        <BookingCard class="w-4/5" />
-        <BookingCard class="w-4/5" />
-      </div>
-      <div class="flex-1 w-1/2 items-start">
-        <ViewBooking class="w-4/5" />
-      </div>
-    </div> -->
-    <!--  <MembershipCard
-      active="1"
-      heading="No membership"
-      subHeading="Pay as you go"
-      bulletPoints={[
-        "Most flexible option.",
-        "Pay whenever you want to book.",
-        "Perfect for newcomers who are looking to try out before committing to a membership.",
-      ]}
-      class="col-span-4"
-    />
-    <MembershipCard
-      active="0"
-      heading="Cleargym One"
-      subHeading="All-inclusive membership"
-      bulletPoints={[
-        "Highest cost savings.",
-        "Unlimited access to all of our facilities.",
-        "One simple monthly payment.",
-        "Cancel at any time.",
-      ]}
-      class="col-span-4"
-    /> -->
   </div>
 </div>
 
