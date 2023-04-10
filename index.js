b359ab3c6c2de504f5732680fe948deff69c6de3
Dragos Popa <pdragos66@gmail.com>
2023-04-10 17:56:27 +0100

    added comments

--- index.js
+++ index.js
@@ -9,33 +9,40 @@ require('dotenv').config();
 
 app.use(cors());
 
+// webiste is always running locally, so success/cancel redirects should also be localhost
 const DOMAIN = 'http://localhost:5173';
 
+// read json request body
 app.use(express.json())
 
+// change discount amount
 app.post('/discount/:discount' , (req, res) => {
   process.env.DISCOUNT = req.params.discount;
   res.status(200).send({"discount": process.env.DISCOUNT})
 })
 
+// get discount amount
 app.get('/discount', (req, res) => {
   res.status(200).send({"discount": process.env.DISCOUNT})
 })
 
-//post add item to basket
+// add item to basket
 app.post('/basket/:userId', async (req, res) => {
   let items = req.body.items;
   
+  // check if user already has a basket
   BasketModel.findByUID(req.body.userId).then((result) => {
       if (!result) {
+        // if not, create new basket
         BasketModel.createBasket({userId: req.body.userId})
       }
     });
 
+    // add items to basket
     BasketItemController.addBasketItem(req, res)
 })
 
-//get basket
+// get basket
 app.get('/basket/:userId', async (req, res) => {
   let prices = [];
 
@@ -45,6 +52,7 @@ app.get('/basket/:userId', async (req, res) => {
       });
 })
 
+// clear a user basket (delete)
 app.delete('/basket/:userId', async (req, res) => {
   BasketModel.removeByUID(req.params.userId)
       .then((result) => {
@@ -52,19 +60,24 @@ app.delete('/basket/:userId', async (req, res) => {
       })
 })
 
+// create stripe checkout session and redirect
 app.post('/checkout/:userId', async (req, res) => {
 
+    // find the users basket
     BasketModel.findByUID(req.params.userId)
     .then(async (basket) => {
 
+      // format user basket items to stripe checkout line items
       let line_items = [];
       let coupon = null;
       let session;
 
+      // check if basket has discount applied
       if (basket.discount == true) {
         coupon = await stripe.coupons.create({percent_off: process.env.DISCOUNT, duration: 'once'});
       }
 
+      // format basket items
       for (let i = 0; i < basket.items.length; i++) {
         
         let product_data = {name: basket.items[i].bookingType + " " + basket.items[i].bookingDate}
@@ -73,14 +86,17 @@ app.post('/checkout/:userId', async (req, res) => {
         
       }    
 
+      // if coupon exists, create a checkout session with a coupon applied
       if (coupon) {
         session = await stripe.checkout.sessions.create({
+          // items formatted before
           line_items,
           discounts: [{
             coupon: coupon.id,
           }],
           //mode: 'subscription',
           mode: 'payment',
+          // redirect URLs
           success_url: `${DOMAIN}/success`,
           cancel_url: `${DOMAIN}/cancel`,
         });
@@ -89,6 +105,7 @@ app.post('/checkout/:userId', async (req, res) => {
           line_items,
           //mode: 'subscription',
           mode: 'payment',
+          // redirect URLs
           success_url: `${DOMAIN}/success`,
           cancel_url: `${DOMAIN}/cancel`,
         });
@@ -98,4 +115,5 @@ app.post('/checkout/:userId', async (req, res) => {
     });
 });
 
+// debug log when starting module
 app.listen(3004, () => console.log('Running on port 3004'));
