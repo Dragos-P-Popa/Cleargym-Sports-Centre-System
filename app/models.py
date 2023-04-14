26087c404d5b321eb9b5319e9a6c3626bbf3d57c
Dragos Popa <pdragos66@gmail.com>
2023-04-14 14:00:26 +0100

    Changed variable names

--- app/models.py
+++ app/models.py
@@ -7,10 +7,10 @@ from datetime import datetime
 class Sales(db.Model):
     __tablename__ = "sales"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    SaleVal = db.Column(db.Float,  nullable=False)
-    Facilityid = db.Column(db.Integer, nullable=False)
-    Activityid = db.Column(db.Integer, nullable=False)
-    SaleDate = db.Column(db.Date, default=datetime.now())
+    saleValue = db.Column(db.Float,  nullable=False)
+    facilityId = db.Column(db.Integer, nullable=False)
+    activityId = db.Column(db.Integer, nullable=False)
+    saleDate = db.Column(db.Date, default=datetime.now())
 
 
 
